e34d03b27fe0151311e1dec0e154bd8ae17e6660
SangYoung Lee <leesy24@gmail.com>
2019-12-04 17:09:11 +0900

    update to validate exist symbolic links

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ln -s libpcap.so.1.8.1 libpcap.so.1
-ln -s libssp.so.0.0.0 libssp.so.0
+[ ! -h libpcap.so.1 ] && ln -sf libpcap.so.1.8.1 libpcap.so.1
+[ ! -h libssp.so.0 ] && ln -sf libssp.so.0.0.0 libssp.so.0
 
 export LD_LIBRARY_PATH=/flash
